fcc5cd15778360b7489b96d8e9e9654aae412cdf
JM Baldonado <john.baldonado@xurpas.com>
2023-02-10 19:50:45 +0800

    connect to an API and map the value of the response in relation to the needs of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MovieList from "./components/Movie/MovieList";
 
@@ -17,13 +17,33 @@ function App() {
       releaseDate: "2021-05-19",
     },
   ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+
+        setMovies(transformedMovies);
+      });
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={dummyMovies} />
+        <MovieList movies={movies} />
       </section>
     </React.Fragment>
   );
